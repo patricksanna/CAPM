c1dbd588e2b1445c2e29498fa5d59d62136bd601
nickmoen <nt.moen.808@gmail.com>
2015-11-05 19:19:15 -0500

    Added log returns and regression to market.

--- server.R
+++ server.R
@@ -8,8 +8,12 @@ shinyServer(function(input, output) {
     prices <- getSymbols(input$symb, auto.assign = FALSE)
     market <- getSymbols("^GSPC", auto.assign = FALSE)
     dataPrices <- merge.xts(Ad(prices), Ad(market), join = "inner")
-
-    
+    stock <- dataPrices[,1]
+    mm <- dataPrices [,2]
+    sReturns <- Delt(stock)[-1]
+    mmReturns <- Delt(mm)[-1]
+    lm(as.vector(sReturns) ~ as.vector(mmReturns))
+      ##CAPM plot needed to show risk and expected return of the asset.
   })
   
 
