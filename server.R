347b96eef4a348499c13be489f32821fa072bc88
Matt Brigida <matt@complete-markets.com>
2015-11-14 10:43:10 -0500

    A minor change and some notes.

--- server.R
+++ server.R
@@ -16,7 +16,7 @@ shinyServer(function(input, output) {
     rMM <- sapply(mmReturns, mean)*100
     rF <- .0047
       ##Risk Free rate as of 11/06/2015 for 1 year bond (maybe a way to not hardcode?)
-    rI <- rF +  reg$coefficients * (rMM - rF)
+    rI <- rF +  reg$coef[2] * (rMM - rF)
     theData <- list(x = data.frame(cbind(as.vector(sReturns), as.vector(mmReturns))), y = reg, z = rI)
   })
   
@@ -24,10 +24,12 @@ shinyServer(function(input, output) {
   output$plot <- renderPlot({
     
     theData <- dataInput()
-   ## need to label the axes below and add the equation to the table, also may want to use ggplot ----
-    plot(theData$x[,2], theData$x[,1])
-    abline(theData$y$coef[1], theData$y$coef[2])
-    qplot(theData$y$coef[2], theData$z[2], ylim = c(0, .20), xlim = c(0, 2), ylab = "Expected Return", xlab = "Beta")
+      ## need to label the axes below and add the equation to the table, also may want to use ggplot ----
+### commenting below, it is just overwritten by qplot ----
+    ## plot(theData$x[,2], theData$x[,1])
+    ## abline(theData$y$coef[1], theData$y$coef[2])
+      qplot(theData$y$coef[2], theData$z, ylim = c(0, .20), xlim = c(0, 2), ylab = "Expected Return", xlab = "Beta")
+      ## we could add the SML -- plot rI for a fange of Beta values ----
       ##Possibly add risk free intercept and the SML. Also need smaller intervals for E[R]
       ##Would a second plot for reg be redundant? 
   })
