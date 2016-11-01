8ec6f78c965e080caabe80f64ead3253267d3f6b
patricksanna <pat.anna@ymail.com>
2016-11-01 19:28:23 -0400

    11/1/16 After Class Updates
    Got rid of rf and Fred data by getting USO instead
    Upon trying to run app obtaining following error NROW(x) must match
    length(order.by) you mentioned it might be an error with the xts file

--- app.R
+++ app.R
@@ -18,7 +18,7 @@ ui <- fluidPage(
 	    ## This sets up two panels -- I think we only want one, the 3D scatterplot.  Should we remove the second panel?  
       tabsetPanel(
         tabPanel("Market Regression", plotOutput("plot"),h4(textOutput('beta'))),
-        tabPanel("Expected Return",  plotOutput("SML"), textOutput("eRmath"), helpText("[eR = rF + Beta(mR - rF)]"))
+        tabPanel("Expected Return",  plotOutput("SML"), textOutput("eRmath"), helpText("[eR =Beta(mR)]"))
         
       )
     ))
@@ -38,26 +38,26 @@ server <- function(input, output) {
     market <- getSymbols("^GSPC", from = input$date[1], to = input$date[2], auto.assign = FALSE)
     
     #options(download.file.method="curl")
-    t <- getSymbols.FRED("TB1YR", auto.assign = FALSE)
+    #t <- getSymbols.FRED("TB1YR", auto.assign = FALSE)
     ## Lets also pull crude oil prices from FRED
-    oil <- getSymbols.FRED("DCOILWTICO", auto.assign = FALSE)
-    dataPrices <- merge.xts(Ad(prices), Ad(market), join = "inner")
+    oil <- getSymbols("USO", auto.assign = FALSE)
+    dataPrices <- merge.xts(Ad(prices), Ad(market), Ad(oil), join = "inner")
     
     stock <- dataPrices[,1]
     mm <- dataPrices [,2]
     sReturns <- Delt(stock)[-1]
     mmReturns <- Delt(mm)[-1]
     oilReturns <- Delt(oil)[-1]
-    data.merged <- merge.xts(mmreturns,oilreturn,sreturns, join="inner")
+    data.merged <- merge.xts(mmReturns,oilReturn,sReturns, join="inner")
     names(data.merged) <- c("market", "oil", "stock")
 
 
     ## wont work yet -- vectors of different lengths -- merge before regression
     reg <- lm((data.merged$stock) ~ (data.merged$market) + data.merged$oil)
     rMM <- mean(mmReturns)*365
-    rF <- as.vector(t[length(t)])/100
-    rI <- rF +  reg$coefficients[2] * (rMM - rF)
-    theData <- list(x = data.frame(cbind(as.vector(data.merged$stock), as.vector(data.merged$market), as.vector(data.merged$oil))), y = reg, z = rI[length(rI)], rF = rF, rMM = rMM)
+    #rF <- as.vector(t[length(t)])/100
+    rI <-   reg$coefficients[2] * (rMM)
+    theData <- list(x = data.frame(cbind(as.vector(data.merged$stock), as.vector(data.merged$market), as.vector(data.merged$oil))), y = reg, z = rI[length(rI)], rMM = rMM)
 
   })
   
@@ -75,13 +75,13 @@ server <- function(input, output) {
   
   output$eRmath <- renderText({
     theData <- dataInput()
-    return(paste("Expected Return:", round(theData$z*100, 2), "%", " = ", round(theData$rF*100, 2), "%", "+", round(theData$y$coefficients[2], 2), "(", round(100*theData$rMM,2), "%", "-", round(theData$rF*100, 2), "%",")"))
+    return(paste("Expected Return:", round(theData$z*100, 2), "%", " = ", round(100, 2), "%", "+", round(theData$y$coefficients[2], 2), "(", round(100*theData$rMM,2), "%", "-", round(100, 2), "%",")"))
   })
   
   output$SML <- renderPlot({
     theData <- dataInput()
     qplot(theData$y$coef[2], theData$z, ylim = c(0, .15), xlim = c(0, 2), ylab = "Expected Return", xlab = "Beta") +
-      geom_abline(intercept =theData$rF, slope = ((theData$z - theData$rF)/theData$y$coef[2])) +
+      geom_abline(intercept =0, slope = ((theData$z)/theData$y$coef[2])) +
       labs(title = "Security Market Line") +
       geom_point(color = "cadetblue4", size = 3.5)
 
